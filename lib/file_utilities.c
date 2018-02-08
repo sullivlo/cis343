e9672ff71ca800fa42dba60ab850d90e8edbff7e
Louis Sullivan <sullivlo@mail.gvsu.edu>
2018-02-08 13:05:02 -0500

    Commented functions

--- lib/file_utilities.c
+++ lib/file_utilities.c
@@ -8,12 +8,18 @@
 #include "gameOfLife.h"
 #include "file_utilities.h"
 
+//read file takes a (char*) filename specified by the user in gameOfLife.c
+//The **buffer is empty the first call
+//When called again buffer will pass the char array of the grid
 int read_file( char* filename, char **buffer ){
-    //reference https://stackoverflow.com/questions/42033932/c-program-to-reverse-content-of-a-file-and-write-that-to-another-file
-    FILE* file1;
-    file1 = fopen(filename, "r");
-    printf("hi");
+    //reference https://stackoverflow.com/questions/42033932/c-program-to-reverse-content-of-a-file-and-write-
+    // that-to-another-file
+    FILE* file1 = fopen(filename, "r");
     //gets the size of the file
+    if(file1 == NULL){
+    	printf("No File Found.\n");
+    	return -1;
+    }
     int size;
 
     fseek(file1,0L, SEEK_END);
@@ -24,40 +30,24 @@ int read_file( char* filename, char **buffer ){
     return size;
 }
 
-
+//File name is specified by the user in gameOfLife.c 
+//Buffer is the char array taken from the users txt file
+//size is from the users txt file when write file was called
 int write_file( char* filename, char *buffer, int size){
     //reference https://stackoverflow.com/questions/42033932/c-program-to-reverse-content-of-a-file-and-write-that-to-another-file
     FILE* file2;
     file2 = fopen(filename, "w");
 
-    for (int k = size - 1; k >= 0; k--) {
+    for (int k = 0; k < size; k++) {
         char* x =  *(&buffer + k);
-        fprintf(file2, "%s", x);
+        fprintf(file2, "%s ", x);
     }
-
     printf("%s", filename);
-    return 1;
-
 
     return 0;
 
-//int read_file(char* filename, char **buffer ){
-//    FILE* f;
-//    int size;
-//    f = fopen(filename, "r");
-//    if(!f){
-//        printf("File not found.");
-//        return 0;
-//    }
-//    fseek(f, 0L, SEEK_END);
-//    size = ftell(f);
-//    *buffer = (char*) malloc(size);
-//    fread(*buffer, size, 1, filename);
-//    fclose(filename);
-//    return size;
-//}
-
-//int write_file(char* filename, char *buffer, int size){
-//   FILE* f;
-//   f = fopen(filename, "w");
 }
+
+// char buffer_overRide(int* x, int* y, int* grid[][y], char**buffer){
+
+// }
