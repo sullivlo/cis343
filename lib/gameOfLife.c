5d4e09dd5788ab644dea9334770add2791f6947d
Louis Sullivan <sullivlo@mail.gvsu.edu>
2018-02-11 16:06:54 -0500

    Update gameOfLife.c

--- lib/gameOfLife.c
+++ lib/gameOfLife.c
@@ -22,6 +22,7 @@
 int main(int argc,char* argv[])
 {
     int fileSize;
+    int LoadFileSize;
     int x;
     int y;
     int i = 0; 
@@ -52,20 +53,33 @@ int main(int argc,char* argv[])
     //Call fileUtility.c to read in the file path in the args
     //Tokenizing the Buffer File.
     fileSize = read_file(fileName, &buffer);
-    
-    
-    // //Creates the grid and populates the grid.
+    x = atoi(strtok(buffer, " \n"));
+    y = atoi(strtok(NULL, " \n"));
+    //Creates the grid and populates the grid.
     int** grid;
-    printf("%s\n", buffer);
-    tokenizer(&x, &y, &buffer, &grid);
+
+    // Malloc the grid.
+    // getMem(&x, &y, &grid);
+    grid = (int**) malloc(x * sizeof(int*));
+    for(int i =0 ; i < y; ++i) {
+        grid[i] = (int*) malloc(y * sizeof(int));
+    }
+    for(int i = 0; i < x; i++){
+        for(int j = 0; j < y; j++){
+            // *(*(grid+i)+j) = atoi(strtok(NULL, " \n"));
+            (*(*(grid+i)+j)) = atoi(strtok(NULL, " \n"));
+        }
+    }
+    printf("%d\n", &buffer);
     free(buffer);
-    printf("%s\n", buffer);
+    printf("%d\n", &buffer);
     drawGrid(&x, &y, grid); 
     printf("Hit P to start.\n");
     scanf("%c", &save);
     resp = save;
     while(toupper(quit)!= 'Q'){  
-        printf("%c\n", resp);
+        //printf("%c\n", save);
+       // printf("%s\n", resp);
         //Drawing the grid.
         //Check to see if User wants to save before evolution.
         //If yes enters into process to write the file.
@@ -76,33 +90,42 @@ int main(int argc,char* argv[])
             char* newFile;
             printf("What would you like the file to be saved as?\n");
             scanf("%s", &newFile);
-            newBuff(&x, &y, &buffer, grid);
-            write_file(&newFile, buffer, fileSize);
+            puts(&newFile);
             prompt(&save);
             resp = save;
+            write_file(newFile, buffer, fileSize);
         }
-        else if(toupper(resp) == 'L'){
-            printf("Hi2\n");
+        else if((toupper(resp)) == 'L'){
             buffer_overRide(&x, &y, &fileSize, grid, &buffer);
-            printf("%s\n", buffer);
+            printf("%s\n", "What is the file named? ");
+            //prompt(&save);
+            char * LoadFileName;
+            scanf("%s", &LoadFileName);
+            
+             
+            LoadFileSize = read_file(&LoadFileName, &buffer);
+            printf("%s\n", "after load");
             prompt(&save);
             resp = save;
 
         }
         else if(toupper(resp) == 'P'){
-            evolution(&x, &y, &grid);
+            int* tempGrid;
+            evolution(&x, &y, &grid, &tempGrid);
             drawGrid(&x, &y, grid);
             prompt(&save);
             resp = save;
         }
-        else{
-            printf("Not a valid command. Try again.");
-            prompt(&save);
-            resp = save;
+         else  {
+            prompt("please try again");
+            save = 'Wrong';
+            scanf("%c", &save);
         }
         
     }
+    printf("%s\n", "____________    in main     ________________");
     freeMem(&x, &y, &grid);
+    free(buffer);
 
     return 0;
 }
