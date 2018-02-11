a4d6e37e6559c53e12967b6f1c16ac1144f13619
Louis Sullivan <sullivlo@mail.gvsu.edu>
2018-02-11 16:08:07 -0500

    Update file_utilities.c

--- lib/file_utilities.c
+++ lib/file_utilities.c
@@ -34,20 +34,33 @@ int read_file( char* filename, char **buffer ){
     fclose(file1);
     return size;
 }
+/*
+int paint_file(char* fileName, int*** grid, int size){
+  File* filePainter;
+  filePainter = fopen(&filePainter, "w");
+
+  printf("%s %s\n", "Opening file ", &filePainter );
+
+}
+*/
 
 //File name is specified by the user in gameOfLife.c 
 //Buffer is the char array taken from the users txt file
 //size is from the users txt file when write file was called
 int write_file( char* filename, char *buffer, int size){
     //reference https://stackoverflow.com/questions/42033932/c-program-to-reverse-content-of-a-file-and-write-that-to-another-file
-    printf("%s\n", buffer);
+    //puts(&filename);
     FILE* file2;
-    file2 = fopen(filename, "w");
+    file2 = fopen(&filename, "w");
 
-    // char * x =  (buffer + 0);
-    fprintf(file2, "%s ", buffer);
-    printf("%s", filename);
+    printf("%s\n","------- In file util --------" );
+    for (int k = 0; k < size; k++) {
+       
+       char *x =  (buffer+k);
+    
+       fprintf(file2, "%s ", x );
 
+   }
     fclose(file2);
 
     return 0;
